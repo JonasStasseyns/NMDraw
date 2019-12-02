54d920506cd76a49fceaad4d3768c1df0f5f629a
gdm-1718-jonastas <jonastas@student.arteveldehs.be>
2019-12-02 13:03:24 +0100

    canvas to svg thru socket to canvas

--- client/js/main.js
+++ client/js/main.js
@@ -1,62 +1,109 @@
 let can = new fabric.Canvas('c')
 
-
 const socket = io()
-
-socket.on('drawing', (data) => {
-    console.log(data);
-    console.log('any')
-    display.src = data.data;
+//
+// socket.on('drawing', (data) => {
+//     console.log(data);
+//     console.log('any')
+// });
+//
+// const canvas = document.querySelector('.canvas')
+// const context = canvas.getContext('2d')
+// const display = document.querySelector('.display')
+//
+// let currentColor = 'black'
+//
+// canvas.width = window.innerWidth
+// canvas.height = window.innerHeight
+//
+// let isDrawing = false
+//
+// startDrawing = () => {
+//     isDrawing = true
+// }
+//
+// stopDrawing = () => {
+//     isDrawing = false
+//     context.beginPath()
+//     sendCanvas()
+// }
+//
+// drawLine = (e) => {
+//     e.preventDefault()
+//     e.stopPropagation()
+//     if (isDrawing) {
+//         context.lineWidth = 5
+//         context.lineCap = 'round'
+//         context.lineTo(e.clientX, e.clientY)
+//         context.stroke()
+//         context.strokeStyle = currentColor
+//         context.beginPath()
+//         context.moveTo(e.clientX, e.clientY)
+//     }
+// }
+//
+// setColor = (e) => {
+//     currentColor = e.target.className.split(' ')[1]
+// }
+//
+// canvas.addEventListener('mousedown', startDrawing)
+// canvas.addEventListener('mouseup', stopDrawing)
+// canvas.addEventListener('mousemove', drawLine)
+//
+// const colors = document.querySelectorAll('.color')
+// colors.forEach((color) => {
+//     color.addEventListener('click', setColor)
+// })
+//
+// sendCanvas = () => {
+//     socket.emit('drawing', context)
+// }
+// Do some initializing stuff
+fabric.Object.prototype.set({
+    transparentCorners: false,
+    cornerColor: 'rgba(102,153,255,0.5)',
+    cornerSize: 12,
+    padding: 5
 });
 
-const canvas = document.querySelector('.canvas')
-const context = canvas.getContext('2d')
-const display = document.querySelector('.display')
+// initialize fabric canvas and assign to global windows object for debug
+const canvas = window._canvas = new fabric.Canvas('c');
+canvas.setWidth(1000)
+canvas.setHeight(1000)
 
-let currentColor = 'black'
+canvas.add(new fabric.Circle({
+    radius: 50,
+    left: 100,
+    top: 100,
+    fill: '#0B61A4'
+}));
 
-canvas.width = window.innerWidth
-canvas.height = window.innerHeight
+canvas.add(new fabric.Triangle({
+    width: 150,
+    height: 100,
+    left: 160,
+    top: 200,
+    fill: '#00AF64'
+}));
 
-let isDrawing = false
+// Normal SVG output
+fabric.log('Normal SVG output: ', canvas.toSVG());
 
-startDrawing = () => {
-    isDrawing = true
-}
+// SVG output without preamble
+fabric.log('SVG output without preamble: ', canvas.toSVG({
+    suppressPreamble: true
+}));
 
-stopDrawing = () => {
-    isDrawing = false
-    context.beginPath()
-    let drawData
-    canvas.getObjects().clone(function(cloned) {
-        drawData = cloned;
-    });
-    socket.emit('drawing', { data: drawData });
-}
-
-drawLine = (e) => {
-    e.preventDefault()
-    e.stopPropagation()
-    if (isDrawing) {
-        context.lineWidth = 5
-        context.lineCap = 'round'
-        context.lineTo(e.clientX, e.clientY)
-        context.stroke()
-        context.strokeStyle = currentColor
-        context.beginPath()
-        context.moveTo(e.clientX, e.clientY)
+// SVG output with viewBox attribute
+fabric.log('SVG output with viewBox attribute: ', canvas.toSVG({
+    viewBox: {
+        x: 80,
+        y: 80,
+        width: 250,
+        height: 250
     }
-}
+}));
 
-setColor = (e) => {
-    currentColor = e.target.className.split(' ')[1]
-}
-
-canvas.addEventListener('mousedown', startDrawing)
-canvas.addEventListener('mouseup', stopDrawing)
-canvas.addEventListener('mousemove', drawLine)
-
-const colors = document.querySelectorAll('.color')
-colors.forEach((color) => {
-    color.addEventListener('click', setColor)
+document.querySelector('body').addEventListener('click', (evt) => {
+    socket.emit('drawing', canvas.toSVG())
 })
