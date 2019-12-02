a0d868080b282c2b106d75acce5c3f8639c7fc94
gdm-1718-brundeco <32094744+gdm-1718-brundeco@users.noreply.github.com>
2019-12-02 11:19:35 +0100

    chaos

---
+++ client/js/fabric.js
@@ -0,0 +1,11 @@
+console.log(fabric);
+
+// let canvas = new fabric.Canvas('c')
+
+// let rect = new fabric.Rect({
+//     left: 100,
+//     top: 100,
+//     fill: 'red',
+//     width: 200,
+//     height: 200
+// })
